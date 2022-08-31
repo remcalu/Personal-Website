ff30b25b243af41694d0e61ca2d99721dcce4d0f
Remus Calugarescu <remcalu@outlook.com>
2022-08-31 02:40:45 -0400

    First commit

---
+++ precache-manifest.0326c706a59c3fadad4e3608dd578c88.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e7fa8b103014f8f679ef447d0943ae",
+    "url": "/Personal-Website/index.html"
+  },
+  {
+    "revision": "d117a70a1765ac161b46",
+    "url": "/Personal-Website/static/css/main.74700fbd.chunk.css"
+  },
+  {
+    "revision": "e146e93141337c388206",
+    "url": "/Personal-Website/static/js/2.0845388d.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Personal-Website/static/js/2.0845388d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d117a70a1765ac161b46",
+    "url": "/Personal-Website/static/js/main.a5ced447.chunk.js"
+  },
+  {
+    "revision": "a0853f211dd895eea97b",
+    "url": "/Personal-Website/static/js/runtime-main.dce4d7f5.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/Personal-Website/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/Personal-Website/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/Personal-Website/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "fa931abdda35cc5a22feca7bf88cc353",
+    "url": "/Personal-Website/static/media/job2.fa931abd.png"
+  },
+  {
+    "revision": "db0d6bafae11065426eacdc9e37bc7de",
+    "url": "/Personal-Website/static/media/job3.db0d6baf.png"
+  },
+  {
+    "revision": "9923fedf5df7473d96e91d1687780d38",
+    "url": "/Personal-Website/static/media/project1.9923fedf.png"
+  },
+  {
+    "revision": "406c326ae37015d73319d24cc19134d3",
+    "url": "/Personal-Website/static/media/project2.406c326a.gif"
+  },
+  {
+    "revision": "2e1f001b7019aae13a9e14dff8d8d4a3",
+    "url": "/Personal-Website/static/media/project3.2e1f001b.gif"
+  },
+  {
+    "revision": "f5e05cde6c89f2c36a0d4dc234d6f250",
+    "url": "/Personal-Website/static/media/project4.f5e05cde.jpg"
+  },
+  {
+    "revision": "105b82cf0c549e0cd8811406fc87ddcb",
+    "url": "/Personal-Website/static/media/project6.105b82cf.gif"
+  },
+  {
+    "revision": "18606b807db10df689ee762db6611a5d",
+    "url": "/Personal-Website/static/media/project7.18606b80.gif"
+  },
+  {
+    "revision": "1c59bbfc21d12a35224938cc45cffcfc",
+    "url": "/Personal-Website/static/media/self1.1c59bbfc.png"
+  },
+  {
+    "revision": "79dd152eb350364561613146094c62ef",
+    "url": "/Personal-Website/static/media/self2.79dd152e.png"
+  }
+]);
